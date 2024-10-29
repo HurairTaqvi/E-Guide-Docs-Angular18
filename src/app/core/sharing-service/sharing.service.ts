f3c0e14616c79c09ff552819e8dedad00f99b821
Hurair Taqvi <hurairtaqvi@gmail.com>
2024-10-29 18:43:07 +0500

    LiveServer-&-SQAServer-Links-Updated

--- src/app/core/sharing-service/sharing.service.ts
+++ src/app/core/sharing-service/sharing.service.ts
@@ -5,33 +5,40 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class SharingService {
-  private singleService = 'json/services.json';
-  private contentService = 'json/content.json';
-  private loginCredentials = 'json/login-credentials.json'; // Path to login credentials JSON file
-  private DocumentTitleAPI =
-    'http://172.16.100.111:8006/api/MRide/DocumentTitle';
-  private ContentAPI = 'http://172.16.100.111:8006/api/MRide/GetDocument';
+  LiveLink = 'http://192.168.10.63:8006/';
+  SQALink = 'http://172.16.100.111:8006';
+
+  // //JSON
+  //  private singleService = 'json/services.json';
+  // private contentService = 'json/content.json';
+  private loginCredentials = 'json/login-credentials.json';
+
+  // //APIs
+  private DocumentTitleAPI = this.SQALink + '/api/MRide/DocumentTitle';
+  private ContentAPI = this.SQALink + '/api/MRide/GetDocument';
+
+  // private DocumentTitleAPI = 'http://172.16.100.111:8006/api/MRide/DocumentTitle';
+  // private ContentAPI = 'http://172.16.100.111:8006/api/MRide/GetDocument';
 
   constructor(private http: HttpClient) {}
-  //JSON for Docs Title
+  // //JSON Files Calling
   // getServiceData() {
   //   return this.http.get<any[]>(this.singleService);
   // }
 
-  getDocumentTitle() {
-    return this.http.get<any[]>(this.DocumentTitleAPI);
-  }
-
-  //JSON for Content
   // getContentData() {
   //   return this.http.get<any[]>(this.contentService);
   // }
+
+  //API Files Calling
+  getDocumentTitle() {
+    return this.http.get<any[]>(this.DocumentTitleAPI);
+  }
 
   getApiData() {
     return this.http.get<any[]>(this.ContentAPI);
   }
 
-  // Method to get login credentials
   getLoginCredentials() {
     return this.http.get<any[]>(this.loginCredentials);
   }
